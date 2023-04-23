74b64f092a737cc259682c464c8b18c6bb387413
Olesay <selen04ka@pisem.net>
2023-04-24 01:30:44 +0300

    dz1

--- task/task04/Program.cs
+++ task/task04/Program.cs
@@ -2,3 +2,19 @@
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22*/
+Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int maxNumber = number1;
+if (number2 > maxNumber)
+{
+    maxNumber = number2;
+}
+if (number3 > maxNumber)
+{
+    maxNumber = number3;
+}
+Console.WriteLine($"{maxNumber} максимальное число");
---
+++ task/task06/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+Console.WriteLine("Введите число ");
+int evenNumber = Convert.ToInt32(Console.ReadLine());
+if (evenNumber % 2 > 0)
+{
+    Console.WriteLine($"{evenNumber} нечетное число");
+}
+else 
+Console.WriteLine($"{evenNumber} четное число");
